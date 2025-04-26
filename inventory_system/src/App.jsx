6caef5f41caa23ce2d3de24bc5ec3c7493d74698
Odai Alkhatib <alkhatibo07@lsus.edu>
2025-04-25 23:36:48 -0500

    updated for hosting

--- inventory_system/src/App.jsx
+++ inventory_system/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { useAuth } from './context/AuthContext';
 import { NavigationProvider } from './contexts/NavigationContext';
